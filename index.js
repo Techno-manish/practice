8c68e324b46eb7aab14a1ccb9c3dbe69beb845af
Manish Kumar <manishkumar12340@gmail.com>
2024-04-01 13:42:47 +0000

    updated date format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addDays = require('date-fns/addDays')
 function result(days) {
-  const date = new Date('22 Aug 2020')
+  const date = new Date(2020,7,22)
   let newDate = addDays(date, days)
   day = newDate.getDate()
   month = newDate.getMonth() + 1
